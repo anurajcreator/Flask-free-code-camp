0b0e05c181d8c7bf2130c7a002593e2d16cf2443
anurajcreator <anurajsingh883@gmail.com>
2021-07-09 01:43:54 +0530

    Dynamic route

--- market.py
+++ market.py
@@ -1,6 +1,22 @@
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/about/<username>')
+def about_page(username):
+    return f'<h1>Wellcome {username}</h1>'
+
+
+@app.route('/anuraj')
+def anuraj():
+    return "This is me Mario"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
