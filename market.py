ce18a854f0df59f7d76653daf590aa1c5b09e154
anurajcreator <anurajsingh883@gmail.com>
2021-07-09 12:56:39 +0530

    Styling

--- market.py
+++ market.py
@@ -1,22 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
+
+@app.route('/home')
 def hello_world():
-    return 'Hello, World!'
-
-
-@app.route('/about/<username>')
-def about_page(username):
-    return f'<h1>Wellcome {username}</h1>'
-
-
-@app.route('/anuraj')
-def anuraj():
-    return "This is me Mario"
-
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
